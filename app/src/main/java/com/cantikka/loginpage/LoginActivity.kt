61927bbcf2247ea57ca6a7c18e8f1fab697b74b1
cantikkamay <cantikkamayaristianti@gmail.com>
2025-02-26 08:12:36 +0700

    perubahan LoginActivity.kt dan MainActivity.kt

--- app/src/main/java/com/cantikka/loginpage/LoginActivity.kt
+++ app/src/main/java/com/cantikka/loginpage/LoginActivity.kt
@@ -7,6 +7,7 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
--- app/src/main/java/com/cantikka/loginpage/MainActivity.kt
+++ app/src/main/java/com/cantikka/loginpage/MainActivity.kt
@@ -9,6 +9,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
