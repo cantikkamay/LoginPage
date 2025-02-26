e5ac68ded79e0e0fbee8845e02b582e380a99cd3
cantikkamay <cantikkamayaristianti@gmail.com>
2025-02-26 08:10:29 +0700

    penambahan MainActivity.kt

--- app/src/main/java/com/cantikka/loginpage/MainActivity.kt
+++ app/src/main/java/com/cantikka/loginpage/MainActivity.kt
@@ -9,7 +9,6 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
